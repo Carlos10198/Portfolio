0c620b45552bf59835b9bb50e050f7402e8f50c5
CarlosBueno <161094329+Carlos10198@users.noreply.github.com>
2025-07-21 22:00:39 -0300

    refactor: nome importação

--- Portfolio/src/containers/header/index.jsx
+++ Portfolio/src/containers/header/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Foto from '../../assets/images/perfil.JPG';
+import Foto from '../../assets/images/perfil.jpg';
 import {MyFoto, MyHeader, MyIcon, MyButton} from './style';
 import { Icon } from "@iconify/react";
 import Button from 'react-bootstrap/Button';
